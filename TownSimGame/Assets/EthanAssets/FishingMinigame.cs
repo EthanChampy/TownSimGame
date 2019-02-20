b96f67620097ebb7f689738dae23685771c7494c
EthanChampy <ethanchampion1998@gmail.com>
2019-02-20 14:03:21 +0000

    FishingMinigameScriptStart
    Fishing Minigame Script initiated

---
+++ TownSimGame/Assets/EthanAssets/FishingMinigame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishingMinigame : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
